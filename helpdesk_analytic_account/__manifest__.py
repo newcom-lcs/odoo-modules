6b8a76bcc613714e0cd825bac36d52600e0434ff
Manuel Nuñez <89217597+mnunez-newcom@users.noreply.github.com>
2024-12-17 19:37:09 +0000

    feat: helpdesk analytic account

--- helpdesk_analytic_account/__manifest__.py
+++ helpdesk_analytic_account/__manifest__.py
@@ -2,9 +2,9 @@
 {
     'name': 'Help Desk Update Analytic Account to Timesheets',
     'license': 'LGPL-3',
-    'version': '16.0.1',
+    'version': '16.0.2',
     'category': 'Stock',
-    'depends': ['timesheet_grid', 'helpdesk' ],
+    'depends': ['timesheet_grid', 'helpdesk', 'helpdesk_timesheet' ],
     'data':[ 
         'views/helpdesk_ticket_views.xml',
      ],

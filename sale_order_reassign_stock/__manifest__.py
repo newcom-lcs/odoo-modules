dcd4ce4b8bfba68b766b280bf902e71e6a0bb539
Manuel Olavarriaga <olavrriagam@gmail.com>
2025-03-23 18:10:09 -0300

    depends on permisos_equipos_ventas

--- sale_order_reassign_stock/__manifest__.py
+++ sale_order_reassign_stock/__manifest__.py
@@ -5,7 +5,7 @@
     'license': 'LGPL-3',
     'author': 'Newcom LCS',
     'category': 'Sales',
-    'depends': ['base',  "stock", 'sale_management'],
+    'depends': ['base',  "stock", 'sale_management', 'permisos_equipos_ventas'],
     
     'data': [
         "views/wizard_views.xml",
